7e5587c2324abe3622a8244795ee665929a9ed88
Pramodya Deshan <92201675+pdeshan@users.noreply.github.com>
2022-01-21 01:06:27 +0530

    Change Js file
    Change js file codes

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,76 @@
 
+    var num = parseInt(document.getElementById('myNumber').value); //get value for variable after convert to integer
 
-var num = document.getElementById('myNumber').value;
+    //Increment
+    function inc()
+    {
+        num = num+1; // number = number+1
+        document.getElementById('valueText').innerHTML = num; //show value
 
-function inc()
-{alert(num)
-    num = num+1;
-    document.getElementById('number').value = num;
+    }
 
-}
+    //Decrement
+    function dec()
+    {
+        num = num-1; // number = number-1
+        document.getElementById('valueText').innerHTML = num; //show value
+
+    }
+
+    //Multiple X 2
+    function mul2()
+    {
+        num = num+2; // number = number X 2
+        document.getElementById('valueText').innerHTML = num; //show value
+
+    }
+
+    //Multiple X 3
+    function mul3()
+    {
+        num = num+3; // number = number X 3
+        document.getElementById('valueText').innerHTML = num; //show value
+
+    }
+
+    //Multiple X 4
+    function mul4()
+    {
+        num = num+4; // number = number X 4
+        document.getElementById('valueText').innerHTML = num; //show value
+
+    }
+
+    //Multiple X 5
+    function mul5()
+    {
+        num = num+5; // number = number X 5
+        document.getElementById('valueText').innerHTML = num; //show value
+
+    }
+
+    //Multiple X 6
+    function mul6()
+    {
+        num = num+6; // number = number X 6
+        document.getElementById('valueText').innerHTML = num; //show value
+
+    }
+
+    //Multiple X 7
+    function mul7()
+    {
+        
+        num = num+7; // number = number X 7
+        document.getElementById('valueText').innerHTML = num; //show value
+
+    }
+
+    //Reset Value
+    function resetBtn()
+    {
+        
+        num = 0; // number = 0
+        document.getElementById('valueText').innerHTML = num; //show value
+
+    }
